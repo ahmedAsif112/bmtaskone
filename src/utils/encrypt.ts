8f2c54b7dce30ea5827fe22339cda9f07f50d48d
Ahmed <ma2877660@gmail.com>
2026-07-02 11:43:37 +0500

    removed some files

--- src/utils/encrypt.ts
+++
@@ -1,7 +0,0 @@
-import CryptoJS from "crypto-js";
-
-export const encryptPassword = (password: string) => {
-    const secret = process.env.NEXT_PUBLIC_PASSWORD_SECRET_KEY!;
-
-    return CryptoJS.AES.encrypt(password, secret).toString();
-};
